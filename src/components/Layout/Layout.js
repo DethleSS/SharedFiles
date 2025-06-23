a1ce82e493f200c81b5b95b4d179562addf4aadb
DethleSS <44126232+DethleSS@users.noreply.github.com>
2025-06-23 13:08:26 +0200

    Update src/components/Layout/Layout.js

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,110 @@
+const topics = [
+    {
+        id: 1,
+        image: "https://wallpapercave.com/wp/wp6193799.jpg",
+        nameRace: "Rising Sun GT3 - Spa Francorchamps",
+        country: "Belgium",
+        imageCountry: "https://cdn.countryflags.com/thumbs/belgium/flag-3d-round-250.png",
+        dateRace: "Sunday, 01 November, 2020 | 21:00",
+        background_image: "https://images6.alphacoders.com/107/1075592.jpg"
+    },
+    {
+        id: 2,
+        image: "https://images.hdqwalls.com/wallpapers/2020-assetto-corsa-competizione-4k-oi.jpg",
+        nameRace: "Rising Sun GT3 - Laguna Seca",
+        country: "USA",
+        imageCountry: "https://img.pngio.com/america-flag-us-usa-icon-american-flag-icon-png-2000_2000.png",
+        dateRace: "Sunday, 01 November, 2020 | 21:00",
+        background_image: "https://images6.alphacoders.com/107/1075592.jpg"
+    },
+    {
+        id: 3,
+        image: "https://images8.alphacoders.com/107/1075601.jpg",
+        nameRace: "Rising Sun - GT4 Brands Hatch",
+        country: "Great Britain",
+        imageCountry: "https://icons.iconarchive.com/icons/iconscity/flags/256/uk-icon.png",
+        dateRace: "Sunday, 01 November, 2020 | 21:00",
+        background_image: "https://images6.alphacoders.com/107/1075592.jpg"
+    },
+    {
+        id: 4,
+        image: "https://images8.alphacoders.com/107/thumb-1920-1075602.jpg",
+        nameRace: "4Rising Sun GT3 - Spa Francorchamps",
+        country: "Belgium",
+        imageCountry: "https://cdn.countryflags.com/thumbs/belgium/flag-3d-round-250.png",
+        dateRace: "Sunday, 01 November, 2020 | 21:00",
+        background_image: "https://images6.alphacoders.com/107/1075592.jpg"
+    },
+    {
+        id: 5,
+        image: "https://wallpapershome.ru/images/wallpapers/assetto-corsa-competizione-3840x2160-19422.jpg",
+        nameRace: "5Rising Sun GT3 - Laguna Seca",
+        country: "USA",
+        imageCountry: "https://img.pngio.com/america-flag-us-usa-icon-american-flag-icon-png-2000_2000.png",
+        dateRace: "Sunday, 01 November, 2020 | 21:00",
+        background_image: "https://images6.alphacoders.com/107/1075592.jpg"
+    },
+    {
+        id: 6,
+        image: "https://images.hdqwalls.com/wallpapers/assetto-corsa-competizione-2020-ex.jpg",
+        nameRace: "6Rising Sun - GT4 Brands Hatch",
+        country: "Great Britain",
+        imageCountry: "https://icons.iconarchive.com/icons/iconscity/flags/256/uk-icon.png",
+        dateRace: "Sunday, 01 November, 2020 | 21:00",
+        background_image: "https://images6.alphacoders.com/107/1075592.jpg"
+    },
+    {
+        id: 7,
+        image: "https://wallpapershome.ru/images/wallpapers/assetto-corsa-competizione-3840x2160-19702.jpg",
+        nameRace: "7Rising Sun GT3 - Spa Francorchamps",
+        country: "Belgium",
+        imageCountry: "https://cdn.countryflags.com/thumbs/belgium/flag-3d-round-250.png",
+        dateRace: "Sunday, 01 November, 2020 | 21:00",
+        background_image: "https://images6.alphacoders.com/107/1075592.jpg"
+    },
+    {
+        id: 8,
+        image: "https://pbs.twimg.com/media/DiOzzBuVAAAPqa0.jpg",
+        nameRace: "8Rising Sun GT3 - Laguna Seca",
+        country: "USA",
+        imageCountry: "https://img.pngio.com/america-flag-us-usa-icon-american-flag-icon-png-2000_2000.png",
+        dateRace: "Sunday, 01 November, 2020 | 21:00",
+        background_image: "https://images6.alphacoders.com/107/1075592.jpg"
+    },
+    {
+        id: 9,
+        image: "https://www.gtplanet.net/wp-content/uploads/2019/05/Assetto-Corsa-Competizione-Brands-Hatch-04.jpeg ",
+        nameRace: "9Rising Sun - GT4 Brands Hatch",
+        country: "Great Britain",
+        imageCountry: "https://icons.iconarchive.com/icons/iconscity/flags/256/uk-icon.png",
+        dateRace: "Sunday, 01 November, 2020 | 21:00",
+        background_image: "https://images6.alphacoders.com/107/1075592.jpg"
+    },
+    {
+        id: 10,
+        image: "https://www.racedepartment.com/attachments/ac-ferrari-488-gt3-5-jpg.282293/",
+        nameRace: "10Rising Sun GT3 - Spa Francorchamps",
+        country: "Belgium",
+        imageCountry: "https://cdn.countryflags.com/thumbs/belgium/flag-3d-round-250.png",
+        dateRace: "Sunday, 01 November, 2020 | 21:00",
+        background_image: "https://images6.alphacoders.com/107/1075592.jpg"
+    },
+    {
+        id: 11,
+        image: "https://pbs.twimg.com/media/DiOz0cFUcAAddeB.jpg:large",
+        nameRace: "11Rising Sun GT3 - Laguna Seca",
+        country: "USA",
+        imageCountry: "https://img.pngio.com/america-flag-us-usa-icon-american-flag-icon-png-2000_2000.png",
+        dateRace: "Sunday, 01 November, 2020 | 21:00",
+        background_image: "https://images6.alphacoders.com/107/1075592.jpg"
+    },
+    {
+        id: 12,
+        image: "https://www.gtplanet.net/wp-content/uploads/2018/04/Assetto-Corsa-Competizione-April-Screenshots-06.jpg",
+        nameRace: "12Rising Sun - GT4 Brands Hatch",
+        country: "Great Britain",
+        imageCountry: "https://icons.iconarchive.com/icons/iconscity/flags/256/uk-icon.png",
+        dateRace: "Sunday, 01 November, 2020 | 21:00",
+        background_image: "https://images6.alphacoders.com/107/1075592.jpg"
+    }
+]
